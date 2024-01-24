5820dcf438f58c886cafe9dfe77adb4febc55ca2
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-24 12:21:45 -0800

    Add conditional statement to determine if roll gets added to current score

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-function Game(player1, player2) {
-    this.players = { player1, player2 };
-}
 
 function Player(playerNumber, name, currentScore, totalScore) {
     this.playerNumber = playerNumber;
@@ -18,33 +15,46 @@ function holdScore() {
     switchPlayers();
 }
 
-function currentScore(diceRoll) {
+Player.prototype.currentScore = (function (diceRoll) {
 
-}
+});
 
 
 function rollDice() {
     let diceRoll = Math.floor(Math.random() * 6) + 1;
     console.log(diceRoll);
+    if (diceRoll === 1) {
+        sessionGame.players.player1.currentScore = 0;
+    }
+    else {
+        sessionGame.players.player1.currentScore = + sessionGame.players.player1.currentScore + diceRoll;
+    }
+    console.log(sessionGame.players.player1.currentScore);
 }
 
+// Business Logic for Game
+function Game(player1, player2) {
+    this.players = { player1, player2 };
+}
 
-function newGame() {
+function handleFormSubmission(event) {
+    // Prevent Event Default
+    event.preventDefault();
     // Create Players
     const player1Name = document.getElementById("player1Name").value;
     const player2Name = document.getElementById("player2Name").value;
     let player1 = new Player(1, player1Name, 0, 0);
     let player2 = new Player(2, player2Name, 0, 0);
     // Create New Game
-    const sessionGame = new Game(player1, player2);
+    sessionGame = new Game(player1, player2);
     console.log(sessionGame);
-}
+};
 
 
 window.addEventListener("load", function () {
     // Add Event Listener to New Game Button
     const newGameButton = document.querySelector("button#newGame");
-    newGameButton.addEventListener("click", newGame);
+    newGameButton.addEventListener("click", handleFormSubmission);
 
     // Add Event Listener to Roll Dice Buttons
     const rollDiceButton = document.querySelector("button#rollDice");
