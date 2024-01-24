5cc712cb883f612a10c4d95c3e8c1d2ff3de5fce
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-24 09:41:13 -0800

    Add event listener to each Roll Dice Button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 
+function rollDice() {
+    console.log("roll dice");
+}
+
+
 function newGame() {
-    console.log("new game")
+    console.log("new game");
 }
 
 
 window.addEventListener("load", function () {
+    // Add Event Listener to New Game Button
     newGameButton = document.querySelector("button#newGame");
     newGameButton.addEventListener("click", newGame);
+
+    // Add Event Listener to Roll Dice Buttons
+    let rollDiceButtons = document.querySelectorAll(".rollDice");
+    rollDiceButtons.forEach(function (button) {
+        button.addEventListener("click", rollDice);
+    });
 })
